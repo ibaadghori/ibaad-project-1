e391ea5f1e50dc8fc7d3f319a017c8b55a8a1b48
ibaadghori <77236883+ibaadghori@users.noreply.github.com>
2021-01-10 14:57:53 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+})
